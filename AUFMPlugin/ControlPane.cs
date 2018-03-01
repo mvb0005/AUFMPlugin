c25e845e2e9396a741e1ad3c6cd8a22bc810f3d4
Matt Bonsall <Matt@DESKTOP-44OQDAB>
2018-03-01 17:39:29 -0600

    Json parser updated for backend result

--- AUFMPlugin/ControlPane.cs
+++ AUFMPlugin/ControlPane.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Net;
 using Newtonsoft.Json;
 using Autodesk.Navisworks.Api;
+using Newtonsoft.Json.Linq;
 
 namespace AUFMPlugin
 {
@@ -22,26 +23,40 @@ namespace AUFMPlugin
 
         }
 
+        private class Protocol
+        {
+            public int protocol_id { get; set;}
+            public string value { get; set; }
+        }
+
         private void Selection_Changed(object sender, EventArgs e)
         {
             Document doc = Autodesk.Navisworks.Api.Application.ActiveDocument;
             if (doc.CurrentSelection.SelectedItems.Count >= 1)
             {
                 ModelItem selectedItem = doc.CurrentSelection.SelectedItems.ElementAt<ModelItem>(0);
-                this.label1.Text = selectedItem.PropertyCategories.FindCategoryByDisplayName("Element ID").Properties.FindPropertyByDisplayName("Value").Value.ToDisplayString().ToString();
-                this.dataGridView1.Visible = true;
-                this.dataGridView1.Rows.Clear();
+                String partID = selectedItem.PropertyCategories.FindCategoryByDisplayName("Element ID").Properties.FindPropertyByDisplayName("Value").Value.ToDisplayString().ToString();
                 WebClient client = new WebClient();
-                string json = client.DownloadString("https://pastebin.com/raw/aVwZDm7U");
-                Dictionary<string, object> properties = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
-                foreach (string p in properties.Keys)
+                //JObject json = JObject.Parse(client.DownloadString("/api/part/" + partID + "/protocol"));
+                JObject json = JObject.Parse(client.DownloadString("https://pastebin.com/raw/rk8NEyFY")); //w0zSmW4R
+                if (json["Error"] != null)
                 {
-                    Dictionary<string, string> valued = JsonConvert.DeserializeObject<Dictionary<string, string>>(properties[p].ToString());
-                    foreach (string v in valued.Keys)
-                    {
-                        this.dataGridView1.Rows.Add(v, valued[v]);
-                    }
+                    this.label1.Text = json["Error"].Value<String>();
                 }
+                else
+                {
+                    IList<JToken> results = json["protocols"].Children().ToList();
+                    this.dataGridView1.Rows.Clear();
+                    foreach (JToken result in results)
+                    {
+                        Protocol p = result.ToObject<Protocol>();
+                        this.dataGridView1.Rows.Add(p.protocol_id, p.value);
+                    }
+                    this.label1.Text = partID;
+                    this.dataGridView1.Visible = true;
+                }
+
+
             }
             else
             {
